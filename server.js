69e27c06104558a2eeee1787084be25f582fc24a
Benjamin Karp <benjaminkarp@gmail.com>
2022-05-01 23:02:58 -0400

    changed file name to server.js

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ app.get('/', (req, res) => {
     res.send('working')    
 })
 
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 app.listen(port, () =>
 console.log('listening on port 3000'))
